1b5d7ae224981dea6fdadd07dd10ee7f6ea22833
Anilforextra <anilforextra@gmail.com>
2024-11-23 18:02:16 +0545

    Lib/UniquePtr: Fix ident.

--- Lib/UniquePtr.c
+++ Lib/UniquePtr.c
@@ -9,8 +9,8 @@ c_function __init__(capacity : int)
   this->arr = (@TEMPLATED_DATA_TYPE@ *)malloc(capacity * sizeof(@TEMPLATED_DATA_TYPE@));
 
   if (this->arr == NULL) {
-      fprintf(stderr, "Memory allocation failed.\n");
-      exit(EXIT_FAILURE);
+    fprintf(stderr, "Memory allocation failed.\n");
+    exit(EXIT_FAILURE);
   }
 endc_function
 
